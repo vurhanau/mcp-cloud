003a43976d0ab7cde2ddd67ce5a404e501b4c999
Alexander Vurganov <alex.vurganov@gmail.com>
2025-04-15 00:05:02 +0100

    Add graph tests

---
+++ Mcp.Azure/tests/Mcp.Azure.Graph.Tests/AzureGraphToolsTests.cs
@@ -0,0 +1,26 @@
+﻿using Mcp.Azure.Tests.Common;
+using FluentAssertions;
+
+namespace Mcp.Azure.Graph.Tests;
+
+public class AzureGraphToolsTests
+{
+    private readonly AzureTestConfig _config;
+
+    public AzureGraphToolsTests()
+    {
+        _config = AzureTestConfigLoader.FromDotEnv();
+    }
+
+    [Fact]
+    public async Task TestGraphTools()
+    {
+        var tenantId = _config.TenantId;
+        var clientId = _config.ClientId;
+        var clientSecret = _config.ClientSecret;
+        var servicePrincipals = await AzureGraphTools.ListServicePrincipals(tenantId, clientId, clientSecret);
+        servicePrincipals.Should().NotBeNull();
+        servicePrincipals.Should().NotBeEmpty();
+        servicePrincipals.Should().ContainSingle(sp => sp.AppId == _config.ClientId);
+    }
+}
